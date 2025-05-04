26a09023301a657db18f5d6c595f6d578412f093
56duong <nguyenduong07122003@gmail.com>
2025-05-04 21:53:10 +0700

    part 11: set up websocket send/receive message

--- backend/src/main/java/org/chatapp/backend/messagecontent/MessageContentRepository.java
+++ backend/src/main/java/org/chatapp/backend/messagecontent/MessageContentRepository.java
@@ -3,10 +3,12 @@ package org.chatapp.backend.messagecontent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MessageContentRepository extends JpaRepository<MessageContent, UUID> {
     Optional<MessageContent> findTopByMessageRoomIdOrderByDateSentDesc(final UUID messageRoomId);
+    List<MessageContent> findByMessageRoomIdOrderByDateSent(final UUID messageRoomId);
 }
--- backend/src/main/java/org/chatapp/backend/messagecontent/MessageContentService.java
+++ backend/src/main/java/org/chatapp/backend/messagecontent/MessageContentService.java
@@ -3,7 +3,9 @@ package org.chatapp.backend.messagecontent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +20,22 @@ public class MessageContentService {
         return messageContentRepository.findTopByMessageRoomIdOrderByDateSentDesc(messageRoomId)
                 .map(m -> messageContentMapper.toDTO(m, new MessageContentDTO()))
                 .orElse(null);
+    }
+
+
+
+    public List<MessageContentDTO> getMessagesByRoomId(final UUID roomId) {
+        return messageContentRepository.findByMessageRoomIdOrderByDateSent(roomId)
+                .stream()
+                .map(m -> messageContentMapper.toDTO(m, new MessageContentDTO()))
+                .toList();
+    }
+
+
+
+    public MessageContentDTO save(final MessageContentDTO messageContentDTO) {
+        final MessageContent messageContent = messageContentRepository.save(messageContentMapper.toEntity(messageContentDTO, new MessageContent()));
+        return messageContentMapper.toDTO(messageContent, new MessageContentDTO());
     }
 
 }
