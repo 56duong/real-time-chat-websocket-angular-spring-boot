49d42e5a824bca760f9a41a2a22687949d31aa12
56duong <nguyenduong07122003@gmail.com>
2026-01-26 20:36:11 +0700

    part 21: free backend deployment on render

--- backend/src/main/java/org/chatapp/backend/user/UserController.java
+++ backend/src/main/java/org/chatapp/backend/user/UserController.java
@@ -1,13 +1,19 @@
 package org.chatapp.backend.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -60,10 +66,40 @@ public class UserController {
 
 
 
+    @Value("${github.token}")
+    private String githubToken;
+
     @PostMapping("/update-avatar-url")
-    public ResponseEntity<UserDTO> updateAvatarUrl(@RequestParam final String file,
+    public ResponseEntity<?> updateAvatarUrl(@RequestParam final MultipartFile file,
                                                    @RequestParam final String username) {
-        return ResponseEntity.ok(userService.updateAvatarUrl(file, username));
+        // 1. convert file to base64
+        try {
+            String base64 = Base64.getEncoder().encodeToString(file.getBytes());
+
+            // 2. upload url
+            String repo = "real-time-chat-websocket-angular-spring-boot";
+            String owner = "56duong";
+            String newFileName = UUID.randomUUID() + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            String path = "storage/avatar/" + newFileName;
+            // 3. upload
+            String githubApiUrl = "https://api.github.com/repos/" + owner + "/" + repo + "/" + "/contents" + path;
+            String jsonBody = "{\"message\": \"Upload avatar\", \"content\": \"" + base64 + "\"}";
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + githubToken);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.exchange(githubApiUrl, HttpMethod.PUT, entity, String.class);
+            // 4. update database
+            String downloadUrl = new ObjectMapper()
+                    .readTree(response.getBody())
+                    .path("content")
+                    .path("download_url")
+                    .asText();
+            return ResponseEntity.ok(userService.updateAvatarUrl(downloadUrl, username));
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Upload failed: " + e.getMessage());
+        }
     }
 
 }
